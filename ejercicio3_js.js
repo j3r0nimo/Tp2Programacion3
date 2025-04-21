e42ec1148b660b45b34e4d696bd5a423a7fbda8c
Carlos ARCE <131560766+sagwalter@users.noreply.github.com>
2025-04-21 00:13:58 -0300

    Update ejercicio3_js.js

--- ejercicio3_js.js
+++ ejercicio3_js.js
@@ -1,12 +1,12 @@
 function resaltar_parrafos(){   
-    const losParrafos = document.querySelectorAll('p');
+    let losParrafos = document.querySelectorAll('p');
     losParrafos.forEach(p => {
         p.classList.add('resaltado')
     })
 }
 
 function ocultar_parrafos(){
-    const losParrafos = document.querySelectorAll('p');
+    let losParrafos = document.querySelectorAll('p');
     losParrafos.forEach(p => {
         p.classList.add('oculto')
     })
